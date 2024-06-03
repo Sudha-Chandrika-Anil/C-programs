b8f30e0ca3d0945110362bd6c85af0dd9b613987
Sudha-Chandrika-Anil <141058459+Sudha-Chandrika-Anil@users.noreply.github.com>
2024-06-03 23:09:59 +0530

    Add files via upload

---
+++ Reverse.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int reverse(int num)
+{
+    int rem,rev=0;
+    while(num>0){
+        rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;
+    }
+    return rev;
+}
+
+
+int main()
+{   
+    int num,result;
+    scanf("%d",&num);
+    result=reverse(num);
+    printf("%d",result);
+    return 0;
+}
+
